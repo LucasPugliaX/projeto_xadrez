80a353a8a3eeafc4b045be9c606f4ecd4fa04049
Lucas Puglia <lucaspugliadev@gmail.com>
2023-06-26 15:19:56 -0300

    Contagem de Movimentos das Peças

--- src/xadrez/PecaXadrez.java
+++ src/xadrez/PecaXadrez.java
@@ -7,6 +7,7 @@ import tabuleiro.Tabuleiro;
 public abstract class PecaXadrez extends Peca {
 
 	private Cor cor;
+	private int contMovimento;
 
 	public PecaXadrez(Tabuleiro tabuleiro, Cor cor) {
 		super(tabuleiro);
@@ -15,6 +16,18 @@ public abstract class PecaXadrez extends Peca {
 
 	public Cor getCor() {
 		return cor;
+	}
+	
+	public int getContMovimento() {
+		return contMovimento;
+	}
+	
+	public void acrescentaContMovimento() {
+		contMovimento++;
+	}
+	
+	public void descontaContMovimento() {
+		contMovimento--;
 	}
 	
 	public XadrezPosicao getXadrezPosicao() {
